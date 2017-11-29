f084e8906bee22a95927e26f2c3a3a6c9679a8ee
louisgt <louis.gauthier@umontreal.ca>
2017-11-29 15:14:00 -0500

    added plot title customization

--- MSD.R
+++ MSD.R
@@ -27,18 +27,18 @@ time_avg_MSD = cbind(tau,time_avg,time_sd)
 time_avg_MSD = as.data.frame(time_avg_MSD)
 
 a = ggplot(msd, aes(x=factor(FRAME),y=VALUE,group=TRACK,color=factor(TRACK))) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + scale_x_discrete(limits=0:400, breaks = seq(0,400,25)) +
-labs(x = "Time",y="MSD") + ggtitle("MSD - All particles")
+labs(x = "Time",y="MSD") + ggtitle(paste("MSD (All particles) - ",prefix,sep=""))
 a$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
 ggsave(paste(prefix,"_MSD_single_trajectories.png",sep=""), plot=a, width = 11, height = 8.5, dpi=300)
 
-b = ggplot(time_avg_MSD, aes(x=tau,y=time_avg)) + geom_errorbar(aes(ymin=time_avg-time_sd, ymax=time_avg+time_sd, color="grey"), width=.05) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle("MSD - Ensemble average")
+b = ggplot(time_avg_MSD, aes(x=tau,y=time_avg)) + geom_errorbar(aes(ymin=time_avg-time_sd, ymax=time_avg+time_sd, color="grey"), width=.05) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle(paste("MSD (Ensemble average) - ",prefix,sep=""))
 b$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
 ggsave(paste(prefix,"_MSD_ensemble_average_SD.png",sep=""), plot=b, width = 11, height = 8.5, dpi=300)
 
-c = ggplot(time_avg_MSD, aes(x=tau,y=time_avg)) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle("MSD - Ensemble average")
+c = ggplot(time_avg_MSD, aes(x=tau,y=time_avg)) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle(paste("MSD (Ensemble average) - ",prefix,sep=""))
 c$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
 ggsave(paste(prefix,"_MSD_ensemble_average.png",sep=""), plot=c, width = 11, height = 8.5, dpi=300)
 
-d = ggplot(time_avg_MSD, aes(x=tau)) + geom_line(aes(y=time_avg, colour="blue")) + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle("MSD - Ensemble average vs. number of tracks per frame") + geom_line(aes(y = n_per_frame/25, colour = "red")) + scale_y_continuous(sec.axis = sec_axis(~., name = "Number of contributing trajectories (x25)"))
+d = ggplot(time_avg_MSD, aes(x=tau)) + geom_line(aes(y=time_avg, colour="blue")) + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle(paste("Ensemble average MSD vs Number of tracks per frame - ",prefix,sep="")) + geom_line(aes(y = n_per_frame/25, colour = "red")) + scale_y_continuous(sec.axis = sec_axis(~., name = "Number of contributing trajectories (x25)"))
 d$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
 ggsave(paste(prefix,"_MSD_ensemble_average_nb_tracks.png",sep=""), plot=d, width = 11, height = 8.5, dpi=300)
