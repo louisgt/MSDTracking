64f88cb801c26fe73e60f9ea5e1e0a7eab8b560d
louisgt <louis.gauthier@umontreal.ca>
2017-11-30 14:52:00 -0500

    implemented ensemble average

--- MSD.R
+++ MSD.R
@@ -10,35 +10,49 @@ pacman::p_load(ggplot2, reshape2)
 print("Importing MSD data...")
 
 #### IMPORT MELTED DF
-msd = as.data.frame(read.csv(paste(prefix,"_melt.txt",sep=""),sep="\t"))
+
+msd = as.data.frame(read.csv(paste(prefix,"_MSD.txt",sep=""),sep="\t"))
+msd_tau = as.data.frame(read.csv(paste(prefix,"_MSD_tau.txt",sep=""),sep="\t"))
 
 ## Order by frame
-msd = msd[order(msd$FRAME),]
+msd_tau = msd_tau[order(msd_tau$FRAME),]
+msd_single = msd_tau
 
 ## Reconstruct dataframe
-unmelt = dcast(msd,TRACK~FRAME)
+unmelt = dcast(msd_tau,TRACK~FRAME)
 
 frame_loss = 401 - rowSums(is.na(unmelt[,2:ncol(unmelt)]))
-n_per_frame = rowSums(sapply(X=frame_loss,y=seq(1,400), FUN=function(x,y) x > y))
-time_avg = colMeans(unmelt[,-1],na.rm = TRUE)
+n_per_frame = rowSums(sapply(X=frame_loss,y=seq(1,401), FUN=function(x,y) x > y))
+msd_tau = colMeans(unmelt[,-1],na.rm = TRUE)
 time_sd = apply(unmelt[,2:ncol(unmelt)], 2, function(x) sd(x, na.rm=TRUE))
-tau=seq(from=0,to=(n_frames*2.048)-3,by=2.048)
-time_avg_MSD = cbind(tau,time_avg,time_sd)
-time_avg_MSD = as.data.frame(time_avg_MSD)
+tau=seq(from=0,to=(n_frames*2.048)-1,by=2.048)
+MELT_tau = cbind(tau,msd_tau,time_sd)
+MELT_tau = as.data.frame(MELT_tau)
 
-a = ggplot(msd, aes(x=factor(FRAME),y=VALUE,group=TRACK,color=factor(TRACK))) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + scale_x_discrete(limits=0:400, breaks = seq(0,400,25)) +
-labs(x = "Time",y="MSD") + ggtitle(paste("MSD (All particles) - ",prefix,sep=""))
+both_MSD = cbind(msd_tau,msd)
+both_MSD = melt(both_MSD,id="TAU")
+
+a = ggplot(msd_single, aes(x=factor(FRAME),y=VALUE,group=TRACK,color=factor(TRACK))) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + scale_x_discrete(limits=0:400, breaks = seq(0,400,25)) +
+labs(x = "Time",y="MSD tau") + ggtitle(paste("MSD tau (All particles) - ",prefix,sep=""))
 a$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
 ggsave(paste(prefix,"_MSD_single_trajectories.png",sep=""), plot=a, width = 11, height = 8.5, dpi=300)
 
-b = ggplot(time_avg_MSD, aes(x=tau,y=time_avg)) + geom_errorbar(aes(ymin=time_avg-time_sd, ymax=time_avg+time_sd, color="grey"), width=.05) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle(paste("MSD (Ensemble average) - ",prefix,sep=""))
+b = ggplot(MELT_tau, aes(x=tau,y=msd_tau)) + geom_errorbar(aes(ymin=msd_tau-time_sd, ymax=msd_tau+time_sd, color="grey"), width=.05) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD tau") + ggtitle(paste("MSD tau (Time-ensemble average) - ",prefix,sep=""))
 b$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
-ggsave(paste(prefix,"_MSD_ensemble_average_SD.png",sep=""), plot=b, width = 11, height = 8.5, dpi=300)
+ggsave(paste(prefix,"_MSD_time-ensemble_average_SD.png",sep=""), plot=b, width = 11, height = 8.5, dpi=300)
 
-c = ggplot(time_avg_MSD, aes(x=tau,y=time_avg)) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle(paste("MSD (Ensemble average) - ",prefix,sep=""))
+c = ggplot(MELT_tau, aes(x=tau,y=msd_tau)) + geom_line() + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD tau") + ggtitle(paste("MSD tau (Time-ensemble average) - ",prefix,sep=""))
 c$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
-ggsave(paste(prefix,"_MSD_ensemble_average.png",sep=""), plot=c, width = 11, height = 8.5, dpi=300)
+ggsave(paste(prefix,"_MSD_time-ensemble_average.png",sep=""), plot=c, width = 11, height = 8.5, dpi=300)
 
-d = ggplot(time_avg_MSD, aes(x=tau)) + geom_line(aes(y=time_avg, colour="blue")) + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD") + ggtitle(paste("Ensemble average MSD vs Number of tracks per frame - ",prefix,sep="")) + geom_line(aes(y = n_per_frame/25, colour = "red")) + scale_y_continuous(sec.axis = sec_axis(~., name = "Number of contributing trajectories (x25)"))
+d = ggplot(MELT_tau, aes(x=tau)) + geom_line(aes(y=msd_tau, colour="blue")) + theme_bw(base_size = 16) + scale_color_discrete(guide=FALSE) + labs(x="Time (s)",y="MSD tau") + ggtitle(paste("Time-ensemble average MSD vs Number of tracks per frame - ",prefix,sep="")) + geom_line(aes(y = n_per_frame/25, colour = "red")) + scale_y_continuous(sec.axis = sec_axis(~., name = "Number of contributing trajectories (x25)"))
 d$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
-ggsave(paste(prefix,"_MSD_ensemble_average_nb_tracks.png",sep=""), plot=d, width = 11, height = 8.5, dpi=300)
+ggsave(paste(prefix,"_MSD_time-ensemble_average_nb_tracks.png",sep=""), plot=d, width = 11, height = 8.5, dpi=300)
+
+e = ggplot(msd) + geom_line(aes(x=TAU,y=MSD)) + theme_bw(base_size=16) + labs(x="Time (s)",y="MSD") + ggtitle(paste("Ensemble average MSD vs Number of tracks per frame - ",prefix,sep=""))
+e$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
+ggsave(paste(prefix,"_MSD_ensemble_average.png",sep=""), plot=e, width = 11, height = 8.5, dpi=300)
+
+f = ggplot(both_MSD) + geom_line(aes(x=TAU,y=value,group=variable,color=variable),size=0.7) + theme_bw(base_size = 16) + labs(x="Time (s)",y="MSD") + scale_colour_manual(values=c("firebrick1","dodgerblue"))
+f$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
+ggsave(paste(prefix,"_MSD_both.png",sep=""), plot=f, width = 11, height = 8.5, dpi=300)
--- MSD.py
+++ MSD.py
@@ -21,10 +21,44 @@ with open(sys.argv[1]) as f:
 		x_coord[int(line[0])-1].append(float(line[1])*0.16)
 		y_coord[int(line[0])-1].append(float(line[2])*0.16)
 
+### count number of frames per track
+track_length = [0 for i in range(n_tracks)]
+for i in range(n_tracks):
+	track_length[i] = len(x_coord[i])
+
+### Ensemble-averaged MSD - Parry definition
+
+# find length of longest trajectory
+# max_time
+# create array MSD of length max_time
+# then
+# for tau in range(max_time)
+#	n = len(trajectories with length >= tau)
+#	for track in range(n)
+#		sum+= (xi(tau) - xi(0))^2
+#	MSD[tau] = sum/n
+
+MSD = [0 for i in range(planes)]
+for tau in range(planes):
+	nb=0
+	sum=0
+	for track in range(n_tracks):
+		if(track_length[track]>tau):
+			#calculate square displacement for the frame
+			sum+=((x_coord[track][tau]-x_coord[track][0])**2+(y_coord[track][tau]-y_coord[track][0])**2)
+			nb+=1
+	MSD[tau] = (sum/nb)
+
+filename = prefix + "_MSD.txt"
+with open(filename, 'w') as o:
+	o.write("TAU" + "\t" + "MSD" + "\n")
+	for tau in range(planes):
+		o.write(str(tau) + "\t" + str(MSD[tau]) + "\n")
+
 # i indexes the current track
 for i in range(n_tracks):
 	# j indexes the current dt (equal to the number of frames)
-	for j in range(1,len(x_coord[i])):
+	for j in range(0,len(x_coord[i])):
 		# k indexes the current frame
 		sum = 0
 		for k in range(len(x_coord[i])-j):
@@ -34,7 +68,7 @@ for i in range(n_tracks):
 		sum=sum/(len(x_coord[i])-j)
 		dr_sq[i].append(sum)
 
-filename = prefix + "_melt.txt"
+filename = prefix + "_MSD_tau.txt"
 with open(filename, 'w') as o:
 	o.write("TRACK" + "\t" + "FRAME" + "\t" + "VALUE" + "\n")
 	# i = track
