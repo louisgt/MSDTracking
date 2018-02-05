ed80d9abf59bfc029e945010122dd4de94bcd0ed
louisgt <louis.gauthier@umontreal.ca>
2018-02-05 15:46:25 -0500

    fixed binning, removed clutter

--- MSD.R
+++ MSD.R
@@ -14,27 +14,16 @@ print("Importing MSD data...")
 msd = as.data.frame(read.csv(paste(prefix,"_MSD_all.txt",sep=""),sep="\t"))
 msd_bin = as.data.frame(read.csv(paste(prefix,"_MSD_binned.txt",sep=""),sep="\t"))
 msd_tau = as.data.frame(read.csv(paste(prefix,"_MSD_tau.txt",sep=""),sep="\t"))
+msd_single = as.data.frame(read.csv(paste(prefix,"_MSD_single.txt",sep=""),sep="\t"))
 
 n_frames = nrow(msd)
 
-## Order by frame
-# msd_single = msd_tau
-
-## Reconstruct dataframe
-# unmelt = dcast(msd_tau,TRACK~FRAME)
-
-# msd_tau = colMeans(unmelt[,-1],na.rm = TRUE)
-# MELT_tau = cbind(unique(msd_single$FRAME),msd_tau)
-# MELT_tau = as.data.frame(MELT_tau)
-# colnames(MELT_tau)[1]="TAU"
-
-# both_MSD = cbind(msd_tau,msd)
 both_MSD = merge(msd_tau,msd,by="TAU",all=T)
 both_MSD = melt(both_MSD,id="TAU")
 
-# single = ggplot(msd_single, aes(x=as.numeric(factor(FRAME))*frame_int,y=VALUE)) + geom_line() + theme_bw(base_size = 18) + scale_color_discrete(guide=FALSE) + labs(x = "Time (s)",y="MSD tau (um²)") + ggtitle(paste("MSD tau (All particles) - ",prefix,sep=""))
-# single$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
-# ggsave(paste(prefix,"_MSD_single_trajectories.png",sep=""), plot=single, width = 11, height = 8.5, dpi=300)
+single = ggplot(msd_single, aes(x=as.numeric(factor(TAU))*frame_int,y=MSD,color=factor(TRACK))) + geom_line() + theme_bw(base_size = 18) + scale_color_discrete(guide=FALSE) + labs(x = "Time (s)",y="MSD tau (um²)") + ggtitle(paste("MSD tau (All particles) - ",prefix,sep=""))
+single$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
+ggsave(paste(prefix,"_MSD_single_trajectories.png",sep=""), plot=single, width = 11, height = 8.5, dpi=300)
 
 bins = ggplot(msd_bin) + geom_line(aes(x=TAU*frame_int,y=MSD,group=factor(BIN),color=BIN),size=1.2) + theme_bw(base_size=20) + labs(x="Time (s)",y="MSD") + scale_color_viridis(guide=guide_legend(title="Bin number",keywidth = 2, keyheight = 2,override.aes = list(alpha = 1),byrow = TRUE),labels=c("2","4","6","8","10"),breaks=c(2,4,6,8,10)) + ggtitle(paste("Ensemble averaged MSD - Binned by intensity - ",prefix,sep=""))
 bins$theme$plot.margin = unit(c(0.5,1,0.5,0.5),"cm")
--- MSD.py
+++ MSD.py
@@ -6,16 +6,12 @@ import math
 
 planes = int(sys.argv[2])
 n_tracks = int(sys.argv[3])
-prefix = sys.argv[4].replace(' ','')
+prefix = sys.argv[4].replace(' ', '')
 
 #x coordinates vector
 x_coord = [[] for i in range(n_tracks)]
 #y coordinates vector
 y_coord = [[] for i in range(n_tracks)]
-
-#delta r squared displacement vector
-dr_sq = [[] for i in range(n_tracks)]
-
 
 #registers corresponding bin for trajectory i
 track_bin = [0 for i in range(n_tracks)]
@@ -43,28 +39,23 @@ max_frame = max(track_length)
 
 ### Ensemble-averaged MSD
 
-MSD = [[0 for i in range(max_frame)] for j in range(n_bins)]
+MSD_binned = [[0 for i in range(max_frame)] for j in range(n_bins)]
 MSD_single = [[0 for i in range(max_frame)] for j in range(n_tracks)]
 MSD_ens = [0 for i in range(max_frame)]
 
 # looping on all time points
-# in the first case (ensemble-averaged) tau denotes the time point used to compute MSD
-# in the second case (time-averaged) tau denotes the interval (or time step) used
+# tau denotes the time point used to compute MSD
 for tau in range(max_frame):
 
-	sum_all = 0
 	nb = [0 for i in range(n_bins)]
 	sum = [0 for i in range(n_bins)]
-	# dx_sum = [0 for i in range(n_bins)]
-	# dy_sum = [0 for i in range(n_bins)]
-	# sx_sum = [0 for i in range(n_bins)]
-	# sy_sum = [0 for i in range(n_bins)]
 
 	msd = 0
+	single = 0
 	count = 0
 
 	for track in range(n_tracks):
-		if(track_length[track]>tau):
+		if track_length[track] > tau:
 			#calculate square displacement for the frame
 			#xd : x axis displacement
 			#yd : y axis displacement
@@ -74,49 +65,18 @@ for tau in range(max_frame):
 			yd = y_coord[track][tau]-y_coord[track][0]
 			fd = math.sqrt((xd**2)+(yd**2))
 
+			MSD_single[track][tau] = (fd**2)
 			msd += (fd**2)
-			count+=1
+			count += 1
 
-			#sum[track_bin[track]]+=((((x_coord[track][tau]-0.2*tau)-x_coord[track][0])**2)+(((y_coord[track][tau]+0.15*tau)-y_coord[track][0])**2))
-			
-			#MSD_single[track][tau] = ((((x_coord[track][tau]-0.2*tau)-x_coord[track][0])**2)+(((y_coord[track][tau]+0.15*tau)-y_coord[track][0])**2))
-
-			# dx_sum[track_bin[track]]+=((x_coord[track][tau]-0.2*tau)-x_coord[track][0])
-			# dy_sum[track_bin[track]]+=((y_coord[track][tau]+0.15*tau)-y_coord[track][0])
-			# sx_sum[track_bin[track]]+=(((x_coord[track][tau]-0.2*tau)-x_coord[track][0])**2)
-			# sy_sum[track_bin[track]]+=(((y_coord[track][tau]+0.15*tau)-y_coord[track][0])**2)
-			#nb[track_bin[track]]+=1
+			sum[track_bin[track]] += (fd**2)
+			nb[track_bin[track]] += 1
 
 	MSD_ens[tau] = (msd/count)
 
-
 	for b in range(n_bins):
-		if(nb[b] is not 0):
-			MSD[b][tau] = (sum[b]/nb[b])
-			# x_MD[b][tau] = (dx_sum[b]/nb[b])
-			# y_MD[b][tau] = (dy_sum[b]/nb[b])
-			# x_MSD[b][tau] = (sx_sum[b]/nb[b])
-			# y_MSD[b][tau] = (sy_sum[b]/nb[b])
-
-filename = prefix + "_MSD_all.txt"
-with open(filename, 'w') as o:
-	o.write("TAU" + "\t" + "MSD" + "\n")
-	for tau in range(max_frame):
-		o.write(str(tau) + "\t" + str(MSD_ens[tau]) + "\n")
-
-filename = prefix + "_MSD_single.txt"
-with open(filename, 'w') as o:
-	o.write("TRACK" + "\t" + "TAU" + "\t" + "MSD" + "\t" + "INTENSITY" + "\n")
-	for track in range(n_tracks):
-		for tau in range(track_length[track]):
-			o.write(str(track) + "\t" + str(tau) + "\t" + str(MSD_single[track][tau]) + "\t" + str(intensity_single[track][tau]) + "\n")
-
-filename = prefix + "_MSD_binned.txt"
-with open(filename, 'w') as o:
-	o.write("BIN" + "\t" + "TAU" + "\t" + "MSD" + "\n")
-	for b in range(n_bins):
-		for tau in range(max_frame):
-			o.write(str(b+1) + "\t" + str(tau) + "\t" + str(MSD[b][tau]) + "\n")
+		if nb[b] is not 0:
+			MSD_binned[b][tau] = (sum[b]/nb[b])
 
 ### Time-ensemble-averaged MSD
 
@@ -124,14 +84,14 @@ MSD_time = []
 
 # dt indexes the current time step (or interval)
 # range of dt depends on the track
-for dt in range(1,max_frame):
+for dt in range(1, max_frame):
 	track_count = 0
-	cumul=0
+	cumul = 0
 	# loop through each track
 	for track in range(n_tracks):
 			#verify that interval is computable for this track
-			if(track_length[track]>dt):
-				track_count +=1
+			if track_length[track] > dt:
+				track_count += 1
 				dt_count = len(x_coord[track])-dt
 				msdt = 0
 				# k indexes the current frame
@@ -153,6 +113,27 @@ for dt in range(1,max_frame):
 				continue
 
 	MSD_time.append(cumul/track_count)
+
+filename = prefix + "_MSD_all.txt"
+with open(filename, 'w') as o:
+	o.write("TAU" + "\t" + "MSD" + "\n")
+	for tau in range(max_frame):
+		o.write(str(tau) + "\t" + str(MSD_ens[tau]) + "\n")
+
+filename = prefix + "_MSD_single.txt"
+with open(filename, 'w') as o:
+	o.write("TRACK" + "\t" + "TAU" + "\t" + "MSD" + "\t" + "INTENSITY" + "\n")
+	for track in range(n_tracks):
+		for tau in range(track_length[track]):
+			o.write(str(track+1) + "\t" + str(tau) + "\t" + str(MSD_single[track][tau]) +
+			        "\t" + str(intensity_single[track][tau]) + "\n")
+
+filename = prefix + "_MSD_binned.txt"
+with open(filename, 'w') as o:
+	o.write("BIN" + "\t" + "TAU" + "\t" + "MSD" + "\n")
+	for b in range(n_bins):
+		for tau in range(max_frame):
+			o.write(str(b+1) + "\t" + str(tau) + "\t" + str(MSD_binned[b][tau]) + "\n")
 
 filename = prefix + "_MSD_tau.txt"
 with open(filename, 'w') as o:
