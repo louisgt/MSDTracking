651cd2944293b83d0d057eed3e33dad42e67803e
louisgt <louis.gauthier@umontreal.ca>
2018-02-20 10:32:18 -0500

    added field formatting

--- combineFiles.sh
+++ combineFiles.sh
@@ -3,16 +3,27 @@
 
 INFILE=$1
 OUTFILE=$2
+
+#Get number of fields
+   N_FIELDS="$(awk -F"\t" 'NR==3{print NF;exit}' ${INFILE})"
+
+   if [ ${N_FIELDS} -gt 5 ];
+   then
+      awk '{print $1,$2,$3,$4,$7}' ${INFILE} > temp.txt
+      cp -f temp.txt ${INFILE}
+      rm temp.txt
+   fi
     
 if [ -f $OUTFILE ]; then
    echo "File $OUTFILE exists."
    echo "Appending to $OUTFILE."
+
    #Get number of tracks in files
    NEW_TRACKS="$(awk 'NR == 2' ${INFILE} | awk '{print $4}')"
    OLD_TRACKS="$(awk 'NR == 2' ${OUTFILE} | awk '{print $4}')"
    TOTAL_TRACKS=$((${NEW_TRACKS}+${OLD_TRACKS}))
    #Append new tracks to file
-   awk 'NR > 3' ${INFILE} | awk -v N=${OLD_TRACKS} '{print ($1+N),$2,$3,$4,$5,$6,$7}' >> ${OUTFILE}
+   awk 'NR > 3' ${INFILE} | awk -v N=${OLD_TRACKS} '{print ($1+N),$2,$3,$4,$5}' >> ${OUTFILE}
    #update track total
    NEW_HEADER="$(awk 'NR == 2' ${OUTFILE} | awk -v N=${TOTAL_TRACKS} '{print $1,$2,$3,N,$5}')"
    sed -i '.bak' '2s/.*/'"${NEW_HEADER}"'/' ${OUTFILE}
